1c8e228675b835286128bcb8706360404aae1c7f
Quocdat210201 <pqd2102@gmail.com>
2022-04-25 10:08:36 +0700

    create new zing

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+
+
+// const headerSettingList = $('.header__setting-list');
+// const headerSetting = $('.header__setting');
+// const headerOverlay = $('.header__right-overlay');
+// // KHI CLICK SETTING
+//  headerSetting.onclick = function(e) {
+//     headerSetting.classList.toggle('header__setting--active');
+//     headerOverlay.classList.remove('hiden');
+//     // headerSettingList.stopPropagation();
+// }
+// headerSettingList.onclick = function(e) {
+//     e.stopPropagation();
+//     headerOverlay.classList.add('hiden');
+//     headerSetting.classList.remove('header__setting--active');
+// }
+// headerOverlay.onclick = function() {
+//     headerSetting.classList.remove('header__setting--active');
+//     headerOverlay.classList.add('hiden');
+// }
